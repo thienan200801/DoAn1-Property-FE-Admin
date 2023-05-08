ada9e778fec5b976838717ab088cc9910059edc1
Ân Trần Bảo <20520377@ms.uit.edu.vn>
2023-05-08 09:52:11 +0700

    feat: set up saga

---
+++ src/redux/sagas/rootSaga.js
@@ -0,0 +1,33 @@
+import { all } from "redux-saga/effects";
+import * as AdministrativeSaga from "./AdministrativeSaga";
+import * as ContactSaga from "./ContactSaga";
+import * as PostSaga from "./PostSaga";
+import * as PropertySaga from "./PropertySaga";
+import * as UserSaga from "./UserSaga";
+
+export default function* rootSaga() {
+  yield all([
+    // UserSaga
+    UserSaga.followActLogin(),
+    // AdministrativeSaga
+    AdministrativeSaga.followActGetProvinces(),
+    AdministrativeSaga.followActGetDistricts(),
+    AdministrativeSaga.followActGetWards(),
+    // PropertySaga
+    PropertySaga.followActGetProperties(),
+    PropertySaga.followActGetPropertyBySlug(),
+    PropertySaga.followActPostProperty(),
+    PropertySaga.followActUpdateProperty(),
+    PropertySaga.followActDeleteProperty(),
+    // ContactSaga
+    ContactSaga.followActGetContacts(),
+    ContactSaga.followActUpdateContact(),
+    ContactSaga.followActDeleteContact(),
+    // PostSaga
+    PostSaga.followActGetPosts(),
+    PostSaga.followActGetPostBySlug(),
+    PostSaga.followActPostPost(),
+    PostSaga.followActUpdatePost(),
+    PostSaga.followActDeletePost(),
+  ]);
+}
