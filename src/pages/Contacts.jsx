603370f0c748526ed6ee4c21a230bb72cee78c1a
Ân Trần Bảo <20520377@ms.uit.edu.vn>
2023-05-12 21:03:57 +0700

    feat: Add homepage, contact and project post pages

---
+++ src/pages/Contacts.jsx
@@ -0,0 +1,76 @@
+import { Box, CircularProgress, Paper } from "@mui/material";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import ContactDetailModal from "~/components/Contacts/ContactDetailModal";
+import EditContactModal from "~/components/Contacts/EditContactModal";
+import TableContacts from "~/components/Table/TableContacts";
+import * as SagaActionTypes from "~/redux/constants/constantSaga";
+import PageHeading from "~/ui-components/PageHeading";
+
+export default function Contacts() {
+  const { loading } = useSelector((state) => state.LoadingReducer);
+  const { contacts } = useSelector((state) => state.ContactReducer);
+
+  const [openDetail, setOpenDetail] = useState(false);
+  const [openModify, setOpenModify] = useState(false);
+  const [selectedContact, setSelectedContact] = useState(null);
+
+  const dispatch = useDispatch();
+
+  const handleClose = () => {
+    setSelectedContact(null);
+    setOpenModify(false);
+    setOpenDetail(false);
+  };
+
+  const handleDetail = (contact) => {
+    setSelectedContact(contact);
+    setOpenDetail(true);
+  };
+
+  const handleModify = (contact) => {
+    setSelectedContact(contact);
+    setOpenModify(true);
+  };
+
+  useEffect(() => {
+    dispatch({ type: SagaActionTypes.GET_CONTACTS_SAGA });
+  }, []);
+
+  return (
+    <Paper elevation={0}>
+      <PageHeading content="Danh sách thông tin liên hệ" />
+      <Box sx={{ height: "16px" }} />
+      {loading ? (
+        <div
+          style={{
+            width: "100%",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            paddingTop: "4rem",
+          }}
+        >
+          <CircularProgress />
+        </div>
+      ) : (
+        <TableContacts
+          data={contacts}
+          handleDetail={handleDetail}
+          handleModify={handleModify}
+        />
+      )}
+
+      {openDetail && (
+        <ContactDetailModal
+          handleClose={handleClose}
+          contact={selectedContact}
+        />
+      )}
+
+      {openModify && (
+        <EditContactModal handleClose={handleClose} contact={selectedContact} />
+      )}
+    </Paper>
+  );
+}
---
+++ src/services/ContactServices.js
@@ -0,0 +1,47 @@
+import Axios from "axios";
+import {
+  URL_DELETE_CONTACT,
+  URL_GET_CONTACTS,
+  URL_UPDATE_CONTACT,
+} from "./urlAPI";
+import Cookies from "js-cookie";
+
+const token = Cookies.get("token");
+
+const ContactServices = {
+  getContacts: () => {
+    return Axios({
+      url: URL_GET_CONTACTS,
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+  },
+
+  updateContact: (contact) => {
+    return Axios({
+      url: URL_UPDATE_CONTACT(contact.id),
+      method: "PATCH",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      data: {
+        processingStatus: contact.processingStatus,
+        noteByAdmin: contact.noteByAdmin,
+      },
+    });
+  },
+
+  deleteContact: (contactId) => {
+    return Axios({
+      url: URL_DELETE_CONTACT(contactId),
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+  },
+};
+
+export default ContactServices;
